340d7e7c6c997b281644862d498e8482c896dcc8
dopezinho <opugnero@gmail.com>
2022-12-06 12:01:39 -0300

    tabinha ready

--- tabinha.js
+++ tabinha.js
@@ -1,1 +1,20 @@
-tabonera()
+function tabonera() {
+    var num = window.document.getElementById('num')
+    var end = document.getElementById('end')
+    if (num.value.length == 0 || end.value.length == 0) {
+        window.alert('esqueceu de preenche TROXA')
+    } else {   
+        var n = Number(num.value)
+        var e = Number(end.value)
+        var x = 0
+        var r = ''
+        var res = window.document.getElementById('answer')
+        res.innerHTML = `tabinha do ${n} toma: <br>`
+        
+        do {
+            r = n*x
+            res.innerHTML += `${x} x ${n} = ${r} <br>`
+            x++
+        } while (x <= e)
+    }
+ }
